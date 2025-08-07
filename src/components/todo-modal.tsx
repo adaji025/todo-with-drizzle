339ebdcab2b90de167c255982b69f8093e241ba0
Aadji Mukhtar <alfadaji@gmail.com>
2025-08-07 12:30:23 +0100

    completed the project

--- src/components/todo-modal.tsx
+++ src/components/todo-modal.tsx
@@ -10,26 +10,48 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { addTodo } from "@/actions/todo-actions";
+import { addTodo, editTodo } from "@/actions/todo-actions";
 
-import SubmitForm from "./submit-form";
+import { useEffect, useState, useTransition } from "react";
+import { TodoTypes } from "@/types/todo.types";
 
 interface IProps {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedTodo?: TodoTypes | null;
 }
-export function TodoModal({ isOpen, setIsOpen }: IProps) {
-  
+export function TodoModal({ isOpen, setIsOpen, selectedTodo }: IProps) {
+  const [isPending, startTransition] = useTransition();
+  const [todo, setTodo] = useState("");
+
+  console.log(selectedTodo);
+
+  const handleAddTodo = async () => {
+    startTransition(async () => {
+      const id = Math.floor(1000 * Math.random() * 9000);
+
+      await addTodo(id.toString(), todo).then(() => {
+        setIsOpen(false);
+      });
+    });
+  };
+
+  const handleEditTodo = async () => {
+    startTransition(async () => {
+      if (selectedTodo)
+        await editTodo(selectedTodo?.id, todo).then(() => {
+          setIsOpen(false);
+        });
+    });
+  };
+
+  useEffect(() => {
+    if (selectedTodo) setTodo(selectedTodo.text);
+  }, [selectedTodo]);
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <form
-        action={async (formData) => {
-          await addTodo(formData);
-          
-          setIsOpen(false);
-        }}
-      >
+      <form action={handleAddTodo}>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>Add Todo</DialogTitle>
@@ -40,14 +62,29 @@ export function TodoModal({ isOpen, setIsOpen }: IProps) {
           <div className="grid gap-4">
             <div className="grid gap-3">
               <Label htmlFor="name-1">Description</Label>
-              <Input id="name-1" name="name" />
+              <Input
+                id="name-1"
+                name="name"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+              />
             </div>
           </div>
           <DialogFooter>
             <DialogClose asChild>
               <Button variant="outline">Cancel</Button>
             </DialogClose>
-            <SubmitForm />
+            <Button
+              onClick={() => {
+                selectedTodo ? handleEditTodo() : handleAddTodo();
+              }}
+              type="submit"
+              disabled={isPending}
+            >
+              {isPending
+                ? "Saving..."
+                : `${selectedTodo ? "Save Changes" : "Add Todo"}`}
+            </Button>
           </DialogFooter>
         </DialogContent>
       </form>
--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,44 +1,75 @@
 "use client";
-import React from "react";
+import React, { useState, useTransition } from "react";
 import { Card } from "./ui/card";
-import { Edit, Trash2 } from "lucide-react";
+import { Edit, LoaderCircle, Trash2 } from "lucide-react";
 import { TodoTypes } from "@/types/todo.types";
 import { Button } from "./ui/button";
-import { deleteTodo, editTodo, toggleTodo } from "@/actions/todo-actions";
+import { deleteTodo, toggleTodo } from "@/actions/todo-actions";
+import { TodoModal } from "./todo-modal";
 
 interface IProps {
   todo: TodoTypes;
 }
 const Todo = ({ todo }: IProps) => {
-  const handleDeleteTodo = async (id: string) => {
-    await deleteTodo(id);
-  };
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<TodoTypes | null>(null);
+  const [isPending, startTransition] = useTransition();
+  const [loading, startDoneTransition] = useTransition();
 
-  const handleEditTodo = async (id: string) => {
-    const text = "todo edited";
-    await editTodo(id, text);
+  const handleDeleteTodo = async (id: string) => {
+    startTransition(async () => {
+      await deleteTodo(id);
+    });
   };
 
   const handleToggleTodo = async (id: string) => {
-    await toggleTodo(id);
+    startDoneTransition(async () => {
+      await toggleTodo(id);
+    });
   };
   return (
-    <Card className="px-2 sm:px-5 mt-5">
-      <div className="flex justify-between">
-        <h2 className="text-lg font-semibold capitalize">{todo.text}</h2>
-        <div className="flex items-center gap-4">
-          <Button onClick={() => handleToggleTodo(todo.id)}>
-            {todo.done ? "done" : "Undone"}
-          </Button>
-          <button onClick={() => handleEditTodo(todo.id)}>
-            <Edit className="cursor-pointer" />
-          </button>
-          <button onClick={() => handleDeleteTodo(todo.id)}>
-            <Trash2 className="cursor-pointer" color="red" />
-          </button>
+    <>
+      <TodoModal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        selectedTodo={selectedTodo}
+      />
+      <Card className="px-2 sm:px-5 mt-5">
+        <div className="flex justify-between">
+          <h2 className="text-lg font-semibold capitalize">{todo.text}</h2>
+          <div className="flex items-center gap-4">
+            <Button
+              disabled={todo.done}
+              onClick={() => handleToggleTodo(todo.id)}
+            >
+              {loading ? (
+                <LoaderCircle className="animate-spin" />
+              ) : (
+                `${todo.done ? "Done" : "New"}`
+              )}
+            </Button>
+            <button
+              onClick={() => {
+                setSelectedTodo(todo);
+                setIsOpen(true);
+              }}
+            >
+              <Edit className="cursor-pointer" />
+            </button>
+            <button
+              disabled={isPending}
+              onClick={() => handleDeleteTodo(todo.id)}
+            >
+              {isPending ? (
+                <LoaderCircle className="animate-spin" />
+              ) : (
+                <Trash2 className="cursor-pointer" color="red" />
+              )}
+            </button>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 };
 
